eacc95d111b6f1b6263e7d4c629fe5700f58e74d
Enis Terzioğlu <terziogluenis@hotmail.com>
2019-04-03 21:54:28 +0300

    auth ve account servislerinin içerileri doldurulmaya başlandı.

---
+++ src/app/models/auth.ts
@@ -0,0 +1,11 @@
+export interface GuestSession {
+  expires_at: string;
+  guest_session_id: string;
+  success: boolean;
+}
+
+export interface RequestToken {
+  expires_at: string;
+  request_token: string;
+  success: boolean;
+}
